d1b79c4c8f7b445d31ba4777485099ecec082ce9
Williams <willemzy2002@gmail.com>
2021-08-02 08:13:45 +0100

    started project

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- site title -->
+    <title>My Portfoilio</title>
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!-- ionicon link -->
+    <link rel="stylesheet" href="vendor/ionicons-master/docs/css/ionicons.min.css">
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="vendor/fontawesome-free-5.13.0-web/fontawesome-free-5.13.0-web/css/all.min.css">
+    <!-- external css style sheet -->
+    <link rel="stylesheet" href="css/sytle.css">
+
+</head>
+<body>
+
+    <nav class="navbar navbar-light navbar-expand-lg py-3 bg-white shadow-sm">
+        <div class="container">
+            <a href="javascript:void(0)" class="navbar-brand text-primary font-weight-bold">
+                Williams
+            </a>
+
+
+            <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="navbar-collapse collapse" id="menu">
+                <ul class="navbar-nav ml-auto">
+                    <!-- link -->
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            About
+                        </a>
+                    </li>
+
+                    <!-- link -->
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            Works
+                        </a>
+                    </li>
+
+                    <!-- link -->
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            Services
+                        </a>
+                    </li>
+
+                    <!-- link -->
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- jquery link -->
+    <script src="vendor/jQuery/jquery-3.5.1.min.js"></script>
+    <!-- popper js link -->
+    <script src="vendor/popper.js/umd/popper.min.js"></script>
+    <!-- bootstrap link -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
